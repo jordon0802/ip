49d64b65ccce2d1815552e68853addaacb999d35
jordon0802 <jordon.chang.jj@gmail.com>
2025-01-22 23:31:19 +0800

    Rename Duke.java to Dominic.java

---
+++ src/main/java/Dominic.java
@@ -0,0 +1,8 @@
+public class Dominic {
+    public static void main(String[] args) {
+        String name = "Dominic";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
