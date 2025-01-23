a4f74711cd712b80f10c3152f539e878974b7693
jordon0802 <jordon.chang.jj@gmail.com>
2025-01-24 00:08:48 +0800

    Add ToDos.java

---
+++ src/main/java/ToDos.java
@@ -0,0 +1,10 @@
+public class ToDos extends Task {
+    public ToDos(String todo) {
+        super(todo);
+    }
+
+    @Override
+    public String toString() {
+        return "[T] " + super.toString();
+    }
+}
