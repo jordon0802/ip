19fbcdd0a22700cf0a819c3bc1cba9e84a796110
jordon0802 <jordon.chang.jj@gmail.com>
2025-02-02 09:40:12 +0800

    Task subclasses: Implement toFileString() and getValid[Task]() methods

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,3 +1,7 @@
+import exceptions.InvalidKeywordException;
+import exceptions.InvalidKeywordOrderException;
+import exceptions.MissingArgumentException;
+
 public class Events extends Task {
     private final String from;
     private final String to;
@@ -8,9 +12,36 @@ public class Events extends Task {
         this.to = to;
     }
 
+    public static String[] getValidEvent(String input) throws MissingArgumentException, InvalidKeywordOrderException,
+            InvalidKeywordException {
+        if (input.isEmpty()) {
+            throw new MissingArgumentException("");
+        }
+        Task.checkKeyword(input, " /from ");
+        Task.checkKeyword(input, " /to ");
+        if (input.indexOf(" /to ") < input.lastIndexOf(" /from ")) {
+            throw new InvalidKeywordOrderException("");
+        }
+        if (input.substring(0, input.indexOf(" /from ")).trim().isEmpty() || input.indexOf(" /from ") == 0) {
+            throw new MissingArgumentException("");
+        }
+
+        String[] args = new String[3];
+        args[0] = input.substring(0, input.indexOf(" /from "));
+        args[1] = input.substring(input.indexOf(" /from ") + 7, input.indexOf(" /to "));
+        args[2] = input.substring(input.indexOf(" /to ") + 5);
+        return args;
+    }
+
+    @Override
+    public String toFileString() {
+        return "[E]\n" + (super.isMarked() ? "[x]" : "[ ]") + "\n" + super.getTask()
+                + " /from " + this.from + " /to " + this.to + "\n";
+    }
+
     @Override
     public String toString() {
         return "[E] " + (super.isMarked() ? "[x] " : "[ ] ")
-            + super.task + " (from: " + this.from + " to: " + this.to + ")";
+            + super.getTask() + " (from: " + this.from + " to: " + this.to + ")";
     }
 }
