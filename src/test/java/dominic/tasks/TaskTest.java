cca29c59a5ab8cf53f51b689db920c5dd8144dfa
jordon0802 <jordon.chang.jj@gmail.com>
2025-02-16 19:15:44 +0800

    TaskTest.java: Rearrange imports

--- src/test/java/dominic/tasks/TaskTest.java
+++ src/test/java/dominic/tasks/TaskTest.java
@@ -1,10 +1,10 @@
 package dominic.tasks;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 
 public class TaskTest {
     @Test
